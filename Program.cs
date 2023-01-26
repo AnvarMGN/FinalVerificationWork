3f695aa43052ad0f7dad21eee9d4c7628a6f8e31
Anvar <akchalov.a.m@yandex.ru>
2023-01-26 17:55:58 +0500

    Edit solution of task.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         Console.Write($"{i}){array[i]}  ");
     }
+    Console.WriteLine();
 }
 
 void SortingMethod(string[] array1, string[] array2)
@@ -11,13 +12,11 @@ void SortingMethod(string[] array1, string[] array2)
     int indexArray2 = 0;
 
     for(int i = 0; i < array1.Length; i++)
-    {
         if(array1[i].Length <= 3)
         {
             array2[indexArray2] = array1[i];
             indexArray2 ++;
         }
-    }
 }
 
 string[] MyArray = {"hello","2","world",":-)"};
@@ -30,3 +29,4 @@ Console.WriteLine();
 
 SortingMethod(MyArray, ModifiedArray);
 ShowArray(ModifiedArray);
+
